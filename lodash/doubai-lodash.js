16e2f9ff8734fb38c9f70ded3c097d4ed814f0f8
梦 <1144102454@qq.com>
2023-06-02 21:17:21 +0800

    lodash

--- lodash/doubai-lodash.js
+++ lodash/doubai-lodash.js
@@ -2,15 +2,15 @@
 
 var doubai = {
   /**
-   * 
-   * @param {*} array 
-   * @param {*} size 
+   *
+   * @param {*} array
+   * @param {*} size
    * @returns (Array): 返回过滤掉假值的新数组
    */
   chunk: function(array, size) {
     var res = []
     for (var i = 0; i < array.length - 1; i += size) {
-      res.push(ary.slice(i, i + size))
+      res.push(array.slice(i, i + size))
     }
     return res
   }
