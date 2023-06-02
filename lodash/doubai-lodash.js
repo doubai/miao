db06031b312a6a444d508cf8cddb3b32940b02c8
梦 <1144102454@qq.com>
2023-06-02 21:36:37 +0800

    lodash

--- lodash/doubai-lodash.js
+++ lodash/doubai-lodash.js
@@ -1,16 +1,20 @@
 //doubai-lodash.js
 
 var doubai = {
-  /**
-   *
-   * @param {*} array
-   * @param {*} size
-   * @returns (Array): 返回过滤掉假值的新数组
-   */
   chunk: function(array, size) {
     var res = []
     for (var i = 0; i < array.length; i += size) {
       res.push(array.slice(i, i + size))
+    }
+    return res
+  },
+
+  compact: function(array, n) {
+    var res = []
+    for (let i of array) {
+      if (i) {
+        res.push(i)
+      }
     }
     return res
   }
